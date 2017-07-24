49042d739789421453157eb473184c0daf7a7186
david grupp <david@kcura.com>
2017-07-24 00:25:52 -0500

    added click logic

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -6,6 +6,8 @@ var app = function(){
     var $messages  = $("#messages");
     var $input     = $("#message-input");
     var $username  = $("#username");
+    var $totalClicks = $(".total-clicks");
+    var $main = $(".main");
 
     var sanitize = function(html){ return $("<div/>").text(html).html(); }
 
@@ -36,21 +38,29 @@ var app = function(){
         }
     });
 
+    $main.click(function(){
+        console.log("main click chan push new:click");
+        self.chan.push("new:click", {"user": "dwg", "hash": "123abc_hash_asdf", "next_click_hash": "987abc_hash_qwer"});
+    });
+
     self.chan.on("new:msg", function( msg ) {
        console.log("chan on new:msg", msg);
         // $messages.append(messageTemplate(msg));
         //scrollTo(0, document.body.scrollHeight);
     });
 
+    self.chan.on("update:total_clicks", function( msg ) {
+       console.log("chan on update:total_clicks", msg.body.total_clicks);
+       $totalClicks.text(msg.body.total_clicks);
+        // $messages.append(messageTemplate(msg));
+        //scrollTo(0, document.body.scrollHeight);
+    });
+
     self.chan.on("user:entered", function( msg ) {
-        console.log("chan on user:entered", msg);
-        var username = sanitize(msg.user || "anonymous")
-        $messages.append(`<br/><i>[${username} entered]</i>`)
+        console.log("chan on user:entered", msg.user);
+        //var username = sanitize(msg.user || "anonymous")
+        //$messages.append(`<br/><i>[${username} entered]</i>`)
     });
     
-
-    
-
-    
-
 }
+var a = new app();
--- web/channels/fill_channel.ex
+++ web/channels/fill_channel.ex
@@ -1,10 +1,11 @@
 defmodule BucketIsland.FillChannel do
   use Phoenix.Channel
   require Logger
-  
+
   def join("fill:lobby", message, socket) do
     Process.flag(:trap_exit, true)
     :timer.send_interval(5000, :ping)
+    :timer.send_interval(1000, :total_clicks)
     send(self, {:after_join, message})
 
     {:ok, socket}
@@ -19,8 +20,15 @@ defmodule BucketIsland.FillChannel do
     push socket, "join", %{status: "connected"}
     {:noreply, socket}
   end
+
   def handle_info(:ping, socket) do
     push socket, "new:msg", %{user: "SYSTEM", body: "ping"}
+    {:noreply, socket}
+  end
+
+   def handle_info(:total_clicks, socket) do
+    total_clicks = 5554123456789012345678
+    push socket, "update:total_clicks", %{user: "SYSTEM", body: %{"total_clicks": Integer.to_string(total_clicks)}}
     {:noreply, socket}
   end
 
@@ -32,5 +40,10 @@ defmodule BucketIsland.FillChannel do
   def handle_in("new:msg", msg, socket) do
     broadcast! socket, "new:msg", %{user: msg["user"], body: msg["body"]}
     {:reply, {:ok, %{msg: msg["body"]}}, assign(socket, :user, msg["user"])}
+  end
+
+  def handle_in("new:click",%{"hash" => hash, "next_click_hash" => next_click_hash, "user" => user}, socket) do
+    # broadcast! socket, "new:msg", %{user: msg["user"], body: msg["body"]}
+    {:reply, {:ok, %{"next_click_hash": next_click_hash}}, assign(socket, :user, user)}
   end
 end
