7add9098edcb19412a50ef18060a2afe3d175e52
david grupp <david@kcura.com>
2017-07-25 22:29:18 -0500

    added init and other functions to clicktotalscache

---
+++ lib/bucketisland/Models/ClickTotals.ex
@@ -0,0 +1,3 @@
+defmodule BucketIsland.Models.ClickTotals do
+    defstruct [:total_bucket_island, :total_other_island, :total_swamp, :total_forest, :total_plains, :total_mountain]
+end
--- lib/bucketisland/Services/ClickTotalsCache.ex
+++ lib/bucketisland/Services/ClickTotalsCache.ex
@@ -1,72 +1,72 @@
-defmodule OptionCalc.Utilities.GenDataCache do
+defmodule BucketIsland.Services.ClickTotalsCache do
     use GenServer
     
     @timeout 1000
 
     def start_link() do
         # get initiall values
-        {:ok, pid} = GenServer.start_link(__MODULE__, :ok, opts)
+        {:ok, pid} = GenServer.start_link(__MODULE__, :ok, [])
         :timer.send_interval(@timeout, pid, :commit)
         {:ok, pid}
     end
 
-    def handle_call(:totals, _from, ${
-        total_bucket_island: total_bucket_island,
-        total_other_island: total_other_island,
-        total_swamp: total_swamp,
-        total_forest: total_forest,
-        total_plains: total_plains,
-        total_mountain: total_mountain,
-    } = totals) do
+    def increment_bucket_island(pid, increment), do: GenServer.cast(pid, {:increment_bucket_island, increment})
+    def increment_other_island(pid, increment), do: GenServer.cast(pid, {:increment_other_island, increment})
+    def increment_swamp(pid, increment), do: GenServer.cast(pid, {:increment_swamp, increment})
+    def increment_forest(pid, increment), do: GenServer.cast(pid, {:increment_forest, increment})
+    def increment_plains(pid, increment), do: GenServer.cast(pid, {:increment_plains, increment})
+    def increment_mountain(pid, increment), do: GenServer.cast(pid, {:increment_mountain, increment})
+    def totals(pid), do: GenServer.call(pid, :totals)
+    def commit(pid), do: GenServer.cast(pid, :commit)
+
+    def init(_) do
+        {:ok, %BucketIsland.Models.ClickTotals{
+            total_bucket_island: 0,
+            total_other_island: 0,
+            total_swamp: 0,
+            total_forest: 0,
+            total_plains: 0,
+            total_mountain: 0
+            }
+        }
+    end
+
+    def handle_call(:totals, _from, totals) do
         {:reply, totals, totals}
     end
 
-    def handle_cast(:commit, ${
-        total_bucket_island: total_bucket_island,
-        total_other_island: total_other_island,
-        total_swamp: total_swamp,
-        total_forest: total_forest,
-        total_plains: total_plains,
-        total_mountain: total_mountain,
-    } = totals) do
+    def handle_cast(:commit, totals) do
         # commit totals to dynamo
         {:noreply, totals}
     end
 
-    def increment_bucket_island(pid, increment), do GenServer.cast(pid, {:increment_bucket_island, increment})
-    def increment_other_island(pid, increment), do GenServer.cast(pid, {:increment_other_island, increment})
-    def increment_swamp(pid, increment), do GenServer.cast(pid, {:increment_swamp, increment})
-    def increment_forest(pid, increment), do GenServer.cast(pid, {:increment_forest, increment})
-    def increment_plains(pid, increment), do GenServer.cast(pid, {:increment_plains, increment})
-    def increment_mountain(pid, increment), do GenServer.cast(pid, {:increment_mountain, increment})
-
-    def handle_cast({:increment_bucket_island, increment}, ${ total_bucket_island: total_bucket_island } = totals) do
-        updated_totals = Map.update(totals, :total_bucket_island, &(&1 + increment))
+    def handle_cast({:increment_bucket_island, increment}, totals) do
+        updated_totals = Map.update!(totals, :total_bucket_island, &(&1 + increment))
         {:noreply, updated_totals}
     end
 
-    def handle_cast({:increment_other_island, increment}, ${ total_other_island: total_other_island } = totals) do
-        updated_totals = Map.update(totals, :total_other_island, &(&1 + increment))
+    def handle_cast({:increment_other_island, increment}, totals) do
+        updated_totals = Map.update!(totals, :total_other_island, &(&1 + increment))
         {:noreply, updated_totals}
     end
 
-    def handle_cast({:increment_swamp, increment}, ${ total_swamp: total_swamp } = totals) do
-        updated_totals = Map.update(totals, :total_swamp, &(&1 + increment))
+    def handle_cast({:increment_swamp, increment}, totals) do
+        updated_totals = Map.update!(totals, :total_swamp, &(&1 + increment))
         {:noreply, updated_totals}
     end
 
-    def handle_cast({:increment_forest, increment}, ${ total_forest: total_forest } = totals) do
-        updated_totals = Map.update(totals, :total_forest, &(&1 + increment))
+    def handle_cast({:increment_forest, increment}, totals) do
+        updated_totals = Map.update!(totals, :total_forest, &(&1 + increment))
         {:noreply, updated_totals}
     end
 
-    def handle_cast({:increment_plains, increment}, ${ total_plains: total_plains } = totals) do
-        updated_totals = Map.update(totals, :total_plains, &(&1 + increment))
+    def handle_cast({:increment_plains, increment}, totals) do
+        updated_totals = Map.update!(totals, :total_plains, &(&1 + increment))
         {:noreply, updated_totals}
     end
 
-    def handle_cast({:increment_mountain, increment}, ${ total_mountain: total_mountain } = totals) do
-        updated_totals = Map.update(totals, :total_mountain, &(&1 + increment))
+    def handle_cast({:increment_mountain, increment}, totals) do
+        updated_totals = Map.update!(totals, :total_mountain, &(&1 + increment))
         {:noreply, updated_totals}
     end
 end
