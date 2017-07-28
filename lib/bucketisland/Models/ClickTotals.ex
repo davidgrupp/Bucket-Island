75da3be148619926a2b0915350040054e746c245
david grupp <david@kcura.com>
2017-07-28 00:44:14 -0500

    added basic query for reading last item

--- lib/bucketisland/Models/ClickTotals.ex
+++ lib/bucketisland/Models/ClickTotals.ex
@@ -1,4 +1,15 @@
 defmodule BucketIsland.Models.ClickTotals do
     @derive ExAws.Dynamo.Encodable
-    defstruct [:id, :total_bucket_island, :total_other_island, :total_swamp, :total_forest, :total_plains, :total_mountain]
+    defstruct [:id, :total_bucket_island, :total_other_island, :total_swamp, :total_forest, :total_plains, :total_mountain, :total_clicks]
+
+    def update_total_clicks(totals) do 
+        total_clicks
+            = totals.total_bucket_island
+            + totals.total_other_island
+            + totals.total_swamp
+            + totals.total_forest
+            + totals.total_plains
+            + totals.total_mountain
+        Map.update!(totals, :total_clicks, fn x -> total_clicks end)
+    end
 end
--- lib/bucketisland/Repositories/ClicksRepository.ex
+++ lib/bucketisland/Repositories/ClicksRepository.ex
@@ -9,24 +9,31 @@ defmodule BucketIsland.Repositories.ClicksRepository do
     def create(pid, clicks), do: GenServer.cast(pid, {:create, clicks})
 
     def handle_call(:get, _from, clicks) do
-        [first|t] = 
-            ExAws.Dynamo.scan("Clicks", limit: 1)
-            |> ExAws.stream!
-            |> Enum.take 1
+        #[first|_] = 
+        x =    ExAws.Dynamo.query("Clicks",
+                limit: 1,
+                scan_index_forward: false,
+                key_condition_expression: "part = :part",
+                expression_attribute_values: [part: 1],
+                index_name: "part-total_clicks-index")
+            |> ExAws.request!
+        IO.inspect x
 
-        first_clicks = %BucketIsland.Models.ClickTotals{
-            id: first["id"]["S"] |> String.to_integer,
-            total_bucket_island: first["total_bucket_island"]["N"] |> String.to_integer,
-            total_other_island: first["total_other_island"]["N"] |> String.to_integer,
-            total_swamp: first["total_swamp"]["N"] |> String.to_integer,
-            total_forest: first["total_forest"]["N"] |> String.to_integer,
-            total_plains: first["total_plains"]["N"] |> String.to_integer,
-            total_mountain: first["total_mountain"]["N"] |> String.to_integer
-            }
-        {:reply, first_clicks, clicks}
+        #first_clicks = %BucketIsland.Models.ClickTotals{
+        #    id: first["id"]["S"] |> String.to_integer,
+        #    total_bucket_island: first["total_bucket_island"]["N"] |> String.to_integer,
+        #    total_other_island: first["total_other_island"]["N"] |> String.to_integer,
+        #    total_swamp: first["total_swamp"]["N"] |> String.to_integer,
+        #    total_forest: first["total_forest"]["N"] |> String.to_integer,
+        #    total_plains: first["total_plains"]["N"] |> String.to_integer,
+        #    total_mountain: first["total_mountain"]["N"] |> String.to_integer
+        #    }
+        {:reply, x, clicks}
     end
 
     def handle_cast({:create, clicks}, totals) do
+        totals = BucketIsland.Models.ClickTotals.update_total_clicks(totals)
+
         ExAws.Dynamo.put_item("Clicks", clicks)
             |> ExAws.request!
         {:noreply, totals}
