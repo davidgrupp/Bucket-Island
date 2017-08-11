f775eb1bd9bb910b4d35c06a0f9e5d397e41b840
david grupp <david@kcura.com>
2017-08-10 22:55:10 -0500

    added logic to read totals from controller

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,13 @@ defmodule BucketIsland.PageController do
   use BucketIsland.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    [{teams_pid, _}] = Registry.lookup(:bucket_island_registry, :team_selection_cache)
+    team_counts = BucketIsland.Services.TeamSelectionCache.get_team_counts(teams_pid)
+    [{clicks_pid, _}] = Registry.lookup(:bucket_island_registry, :click_totals_cache)
+    click_totals = BucketIsland.Services.ClickTotalsCache.totals(clicks_pid)
+    conn
+    |> assign(:click_totals, click_totals)
+    |> assign(:team_counts, team_counts)
+    |> render("index.html")
   end
 end
