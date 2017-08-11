57e639ae103d3223eb7b52b8645464dae9df22e7
david grupp <david@kcura.com>
2017-08-10 22:43:14 -0500

    added improved team name display

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,11 @@
 defmodule BucketIsland.PageView do
   use BucketIsland.Web, :view
+
+  def get_pretty_type("bucket_island"), do: "Bucket Island"
+  def get_pretty_type("other_island"), do: "Other Islands"
+  def get_pretty_type("forest"), do: "Forest"
+  def get_pretty_type("mountain"), do: "Mountain"
+  def get_pretty_type("plains"), do: "PLains"
+  def get_pretty_type("swamp"), do: "Swamp"
+
 end
